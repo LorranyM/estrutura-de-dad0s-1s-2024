1a7a2aec55c054a7332a3312efc6aca2454dfdca
LorranyM <lorrany.matos1005@gmail.com>
2024-04-23 10:29:40 -0300

    add atividade4

---
+++ atividade4/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lista.h>
+
+int main (int argc, char* argv[]){
+
+    No* H = no('A', NULL);
+    No* n2 = no('B', NULL);
+    No* n3 = no('C', NULL);
+    No* n4 = no('D', NULL);
+    No* n5 = no('E', NULL);
+    No* Hc = copiar_lista(H);
+
+    inserir_no(H, n2);
+    inserir_no(H, n3);
+    inserir_no(H, n4);
+    inserir_no(H, n5);
+
+    imprimir_lista(H);
+    printf("\n copia\n");
+    imprimir_lista(Hc);
+
+    H->proximo_no->valor = 'X';
+    Hc->valor = 'U';
+
+    printf("\n");
+    imprimir_lista(H);
+    printf("\n copia\n");
+    imprimir_lista(Hc);
+
+    liberar_lista(Hc);
+
+    printf("\n qtd = %d", quantidade_nos(H));
+    printf("\n copia qtd = %d", quantidade_nos(Hc));
+
+    printf("\n E existe na lista? %d\n", verificar_existencia(H, 'E'));
+    printf("\n E ocorre quantas vezes na lista? %d\n", verificar_ocorrencias(H, 'E'));
+
+    imprimir_inversa(H);
+    inserir_no_i(&H, no('Z', NULL), 0);
+    printf("\n");
+    imprimir_lista(H);
+
+    exit(0);
+}
