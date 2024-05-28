df5584a4cbdf864f298714c9e470d2aaf46e0bcd
LorranyM <lorrany.matos1005@gmail.com>
2024-05-28 11:11:53 -0300

    add fila

---
+++ fila/queue.h
@@ -0,0 +1,11 @@
+typedef struct QueueNode{
+    char data;
+    struct QueueNode* next;
+} QueueNode;
+
+void enqueue(QueueNode** root, char data);
+char dequeue(QueueNode** root);
+char peek(QueueNode* root);
+int isEmpty(QueueNode* root);
+void display(QueueNode* root);
+
